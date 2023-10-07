153dc6e1bee31edf5521ec553435af4fdeeabd32
mosesfawole <mosesfawole2001@gmail.com>
2023-10-07 10:56:30 +0100

    complete combobox

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -24,7 +24,7 @@ const SearchManufacturer = ({
         );
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative">
           <Combobox.Button className="absolute top-[14px]">
             <Image
@@ -66,7 +66,24 @@ const SearchManufacturer = ({
                       }`
                     }
                   >
-                    {item}
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? "font-medium" : "font-normal"
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? "text-white" : "text-teal-600"
+                            }`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
                   </Combobox.Option>
                 ))
               )}
